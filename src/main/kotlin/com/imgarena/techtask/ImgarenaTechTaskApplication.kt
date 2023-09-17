ac46dd181757042a9ca2b70c1637f2b5d9521d2d
gajaharan <gajaharan@gmail.com>
2023-09-17 07:05:05 +0100

    [GAJ-01] Initial commit

---
+++ src/main/kotlin/com/imgarena/techtask/ImgarenaTechTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.imgarena.techtask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImgarenaTechTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImgarenaTechTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/imgarena/techtask/ImgarenaTechTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.imgarena.techtask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImgarenaTechTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
